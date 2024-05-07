95f16880283b760192f8afd8ca7b219cb68b8b0a
Fachriza Hidaya <fachriza.theblues@gmail.com>
2024-05-07 12:46:03 +0700

    chore: second commit

---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_commerce/pages/home_page.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(25),
+              child: Image.asset(
+                'lib/images/drink.jpg',
+                height: 240,
+              ),
+            ),
+            const SizedBox(
+              height: 48,
+            ),
+            const Text('Coffee Time',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
+            const SizedBox(
+              height: 24,
+            ),
+            const Text(
+              'Feel the harmony in peace',
+              style: TextStyle(fontSize: 16, color: Colors.grey),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 48,
+            ),
+            const SizedBox(
+              height: 48,
+            ),
+            GestureDetector(
+              onTap: () => Navigator.push(
+                  context, MaterialPageRoute(builder: (context) => HomePage())),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.grey[900],
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                padding: const EdgeInsets.all(25),
+                margin: const EdgeInsets.all(25),
+                child: const Center(
+                  child: Text(
+                    'Shop Now',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 16),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
