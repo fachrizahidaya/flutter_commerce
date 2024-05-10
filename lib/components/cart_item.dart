ade3b268fee042ddf51712fd56f0bba050fd63e7
Fachriza Hidaya <fachriza.theblues@gmail.com>
2024-05-10 09:28:19 +0700

    fix:  navbar

--- lib/components/cart_item.dart
+++ lib/components/cart_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_commerce/models/cart.dart';
 import 'package:flutter_commerce/models/coffee.dart';
+import 'package:provider/provider.dart';
 
 class CartItem extends StatefulWidget {
   Coffee coffee;
@@ -10,12 +12,23 @@ class CartItem extends StatefulWidget {
 }
 
 class _CartItemState extends State<CartItem> {
+  void removeItemFromCart() {
+    Provider.of<Cart>(context, listen: false).removeItemFromCart(widget.coffee);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      leading: Image.asset(widget.coffee.imagePath),
-      title: Text(widget.coffee.name),
-      subtitle: Text(widget.coffee.price),
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.grey[100], borderRadius: BorderRadius.circular(8)),
+      margin: const EdgeInsets.only(bottom: 10),
+      child: ListTile(
+        leading: Image.asset(widget.coffee.imagePath),
+        title: Text(widget.coffee.name),
+        subtitle: Text(widget.coffee.price),
+        trailing:
+            IconButton(onPressed: removeItemFromCart, icon: Icon(Icons.delete)),
+      ),
     );
   }
 }
